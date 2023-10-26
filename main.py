ebf3b0e7aaeff5e3a8eb2e5bee094cab5ff59a01
adarshjaiswal042 <141535327+adarshjaiswal042@users.noreply.github.com>
2023-10-26 07:25:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ async def restart_handler(_, m):
 
 @bot.on_message(filters.command(["STRANGE"]))
 async def account_login(bot: Client, m: Message):
-    editable = await m.reply_text(' अरे साले फिर से आ गया माँ चुदा जाके  ')
+    editable = await m.reply_text('HELLO BRO I AM TEXT UPLODERBOT SEND ME TEXT ')
     input: Message = await bot.listen(editable.chat.id)
     x = await input.download()
     await input.delete(True)
